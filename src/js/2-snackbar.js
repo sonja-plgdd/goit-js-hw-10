0343705bd64bc847d9c379f04b8be4859da59083
sonja <signtieva@gmail.com>
2024-10-27 22:52:57 +0200

    fix sanckbar

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -10,36 +10,37 @@ function handleSumbit(event) {
   event.preventDefault();
   const checkedRadio = document.querySelector('input[name="state"]:checked');
   const userInput = document.querySelector('input[name="delay"]');
-  
-  setTimeout(() => {
-    new Promise((resolve, reject) => {
+  const delay = userInput.value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
       if (checkedRadio.value === 'fulfilled') {
-        resolve(`Fulfilled promise in ${userInput.value}ms`);
+        resolve(delay);
       }
       else if (checkedRadio.value === 'rejected') {
-        reject(`Rejected promise in ${userInput.value}ms`
-);
+        reject(delay);
       }
+    }, delay);
+  });
+  promise
+    .then((result) => {
+      iziToast.show({
+        message: `Fulfilled promise in ${result}ms`,
+        backgroundColor: '#59A10D',
+        messageColor: '#fff',
+        iconURL: `${svgFile}#icon-bi_check2-circle`,
+        iconColor: '#fff',
+        position: "topRight"
+      });
     })
-      .then((result) => {
-        iziToast.show({
-          message: result,
-          backgroundColor: '#59A10D',
-          messageColor: '#fff',
-          iconURL: `${svgFile}#icon-bi_check2-circle`,
-          iconColor: '#fff',
-          position: "topRight"
+    .catch((error) => {
+      iziToast.show({
+        message: ` Rejected promise in ${error}ms`,
+        messageColor: '#fff',
+        iconURL: `${svgFile}#icon-octagon`,
+        position: "topRight",
+        backgroundColor: '#ef4040',
       });
-      })
-      .catch((error) => {
-        iziToast.show({
-          message: error,
-          messageColor: '#fff',
-          iconURL: `${svgFile}#icon-octagon`,
-          position: "topRight",
-          backgroundColor: '#ef4040',
-      });
-      })
-  }, userInput.value);
+    })
 }
 
